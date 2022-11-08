1654c7abd37491146f0ab0a19160e34ea5534f0d
Sera91 <serafina.digioia@inaf.it>
2022-11-08 19:37:30 +0100

    new file:   ../example.py 	modified:   __pycache__/CM.cpython-310.pyc 	modified:   __pycache__/Netk.cpython-310.pyc 	modified:   __pycache__/__init__.cpython-310.pyc 	modified:   __pycache__/base.cpython-310.pyc 	modified:   __pycache__/data_utils.cpython-310.pyc 	modified:   ../test_Asia.py

--- test_Asia.py
+++ test_Asia.py
@@ -1,34 +1,50 @@
 import pandas as pd
 import numpy as np
-
+#import timemory
+import time
+import argparse
 import slearn as sl
-
-df = sl.import_example('Asia')
-
-n_obs=len(df['asia'])
-
-df.insert(0, 'index', np.arange(n_obs))
-
-#printing to terminal the dataset
-print(df.head(5))
-
-arr_diff= (df['asia'] - df['tub']).to_numpy()
-print(type(arr_diff))
-#counting differences between asia and tub
-print(len(arr_diff[np.where(arr_diff!= 0)])/n_obs)
-
-#df.to_csv('Asia_10k_n.csv')
-#ASIA_DAG = sl.import_DAG(filepath='asia')
-#df = sl.sampling(ASIA_DAG, n=1000)
+#from timemory.component import PapiVector
 
 
 
-# Structure learning
-#model_HC = sl.structure.learn(df, method='hc', verbose=4)
-#sl.plot(model_HC)
+if __name__ == "__main__":
 
-model_PC = sl.structure.learn(df, method='pc', n_jobs=4, verbose=4, variant="parallel")
-sl.plot(model_PC)#,interactive=True)
+    ts = time.perf_counter()
+
+    df = sl.import_example('Asia')
+    
+    ts = time.perf_counter() - ts
+    print("Elapsed time reading dataset (sec) :  {:12.6f}".format(ts))
+
+    n_obs=len(df['asia'])
+
+    df.insert(0, 'index', np.arange(n_obs))
+
+    #hwc = PapiVector("MY_REGION_OF_INTEREST")  # label when printing
+    #hwc.start()
+
+    t2= time.perf_counter() 
+
+    #printing to terminal the dataset
+    #print(df.head(5))
+    #arr_diff= (df['asia'] - df['tub']).to_numpy()
+    #print(type(arr_diff))
+    #counting differences between asia and tub
+    #print(len(arr_diff[np.where(arr_diff!= 0)])/n_obs)
+
+    #df.to_csv('Asia_10k_n.csv')
+    #ASIA_DAG = sl.import_DAG(filepath='asia')
+    #df = sl.sampling(ASIA_DAG, n=1000)
+
+    # Structure learning
+    #model_HC = sl.structure.learn(df, method='hc', verbose=4)
+    #sl.plot(model_HC)
+
+    model_PC = sl.structure.learn(df, method='pc', n_jobs=4, verbose=4, variant="parallel")
+    #sl.plot(model_PC)#,interactive=True)
+    ts2 = time.perf_counter() - t2
+    print("Elapsed time PC (sec) :  {:12.6f}".format(ts2))
 
 
 
