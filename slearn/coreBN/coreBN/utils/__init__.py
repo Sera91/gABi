d40040483f4a079ee765a654f45bbdcb0e925bc4
Sera91 <serafina.digioia@inaf.it>
2023-04-03 13:18:50 -0400

    new file:   utils/GAMfit.py 	modified:   utils/__init__.py

--- slearn/coreBN/coreBN/utils/__init__.py
+++ slearn/coreBN/coreBN/utils/__init__.py
@@ -3,6 +3,7 @@ from .state_name import StateNameMixin
 from .check_functions import _check_1d_array_object, _check_length_equal
 from .optimizer import optimize, pinverse
 from .utils import get_example_model
+from .GAMfit import GAM_residuals, GAM_residuals_fast
 
 
 __all__ = [
@@ -14,4 +15,6 @@ __all__ = [
     "optimize",
     "pinverse",
     "get_example_model",
+    "GAM_residuals",
+    "GAM_residuals_fast"
 ]
