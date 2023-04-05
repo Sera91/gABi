dd7a3d21ac22031e89177d65a641b143f9274ff2
Sera91 <serafina.digioia@inaf.it>
2023-04-05 03:08:48 -0400

    deleted:    slearn/Inf.py 	deleted:    slearn/Netk.py

--- slearn/coreBN/build/lib/coreBN/metrics/__init__.py
+++
@@ -1,9 +0,0 @@
-from .metrics import correlation_score, log_likelihood_score, structure_score
-from .bn_inference import BayesianModelProbability
-
-
-__all__ = [
-    "correlation_score",
-    "log_likelihood_score",
-    "structure_score",
-]
