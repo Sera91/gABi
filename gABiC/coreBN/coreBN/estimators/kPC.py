2dacc5e4ca755c102fbd4c5ae8a57352e04cdc06
Sera91 <serafina.digioia@inaf.it>
2023-04-07 21:00:32 -0400

    modified:   coreBN/coreBN/estimators/base.py 	modified:   coreBN/coreBN/estimators/kPC.py

--- gABiC/coreBN/coreBN/estimators/kPC.py
+++ gABiC/coreBN/coreBN/estimators/kPC.py
@@ -279,29 +279,30 @@ class kPC(StructureEstimator):
         else:
             raise ValueError(
                 f"ci_test must be either hsic_gamma, hsic_perm or dcc_perm or a function. Got: {ci_test}")
+        variables = self.variables[1:]
         
-        
-        max_cond_vars= len(self.variables)-2
+        max_cond_vars= len(variables)-2
         
         
         print("I have created the moral graph")
         # Step 1: Initialize a fully connected undirected graph
-        graph = nx.complete_graph(n=self.variables, create_using=nx.Graph)
+        graph = nx.complete_graph(n=variables, create_using=nx.Graph)
 
         print("I am reading the data")
 
         
-        if self.data_input is not None:
-            if os.path.isdir(self.data_input):
-                data = dd.read_parquet(self.data_input).head(10000)
-                data.sample(frac=1, random_state=rstate).reset_index()
-                data = data.head(N_sample)
-            else:
-                data = dd.read_csv(self.data_input)
-                data.sample(frac=1, random_state=rstate).reset_index()
-                data = data.head(N_sample)
+        #if self.data_input is not None:
+        #    if os.path.isdir(self.data_input):
+        #        data = dd.read_parquet(self.data_input).head(10000)
+        #        data.sample(frac=1, random_state=rstate).reset_index()
+        #        data = data.head(N_sample)
+        #    else:
+        #        data = dd.read_csv(self.data_input)
+        #        data.sample(frac=1, random_state=rstate).reset_index()
+        #        data = data.head(N_sample)
         if self.data is not None:
            data=self.data
+           print("variables in data:", variables)
         else:
             print('error: missing input data')
             sys.exit()
@@ -326,7 +327,7 @@ class kPC(StructureEstimator):
 
             lim_neighbors = 0
         
-            while not all([len(list(graph.neighbors(var))) < lim_neighbors for var in self.variables]) and lim_neighbors <= max_cond_vars:
+            while not all([len(list(graph.neighbors(var))) < lim_neighbors for var in variables]) and lim_neighbors <= max_cond_vars:
 
                 # Step 2: Iterate over the edges and find a conditioning set of
                 # size `lim_neighbors` which makes u and v independent.
@@ -376,7 +377,7 @@ class kPC(StructureEstimator):
                 if show_progress and SHOW_PROGRESS:
                     pbar.update(1)
                     pbar.set_description(
-                        f"Working for n conditional variables: {lim_neighbors}"
+                        f"Working for n conditional variables: {lim_neighbors}")
 
             
             
@@ -387,7 +388,7 @@ class kPC(StructureEstimator):
             #             or  2. `lim_neighbors` is greater than `max_conditional_variables`.
             while not all(
                 [len(list(graph.neighbors(var))) <
-                 lim_neighbors for var in self.variables]
+                 lim_neighbors for var in variables]
             ):
 
                 def _dask_parallel_fun(u, v):
@@ -457,7 +458,7 @@ class kPC(StructureEstimator):
             # Exit condition: 1. If all the nodes in graph has less than `lim_neighbors` neighbors.
             #             or  2. `lim_neighbors` is greater than `max_conditional_variables`.
             while not all(
-                [len(list(graph.neighbors(var))) < lim_neighbors for var in self.variables]):
+                [len(list(graph.neighbors(var))) < lim_neighbors for var in variables]):
 
                 # Step 2: Iterate over the edges and find a conditioning set of
                 # size `lim_neighbors` which makes u and v independent.
@@ -673,7 +674,7 @@ class kPC(StructureEstimator):
         #        data = dd.read_parquet(self.data_input).head(2000)
         #else:
         #        data = dd.read_csv(self.data_input).head(2000)
-	data=self.data
+        data=self.data
         print("selected method for CI test is:", sel_method)
         residuals_matrix = np.zeros(data.shape, dtype=np.float64)
         # I will use a mapping array to convert string into col index
