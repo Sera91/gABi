1aad0c94358ccd2ed7f9841bfd0b4f8f07b359c8
Sera91 <serafina.digioia@inaf.it>
2024-12-10 19:58:39 +0100

    deleted:    gABiC/BN_frontend.py 	deleted:    gABiC/CM.py

--- gABiC/MPIruns/params_basic_5000.py
+++
@@ -1,9 +0,0 @@
-N = 5000
-rstate = 2
-output_dir = "parallel-benchmark/"
-#input_file = "input_data/TNG300_new_normalized_fromv1_wo_mstar9_no_collinear.csv"
-#input= "data_TNG300_disk_2p/"
-#input= "micols_data_TNG300_disk_2p_wo_fdisk/"
-input= "../datasets_input/basic_example_data_30000.csv"
-file_adjlist = output_dir + "file_adjlist-" + str(N) + "-basic-model_rstate_" + str(rstate) + "_GPU.txt"
-file_edgelist= output_dir +"file_edgelist-" + str(N) + "-basic-model_rstate_"+ str(rstate) +"_GPU.gz"
--- gABiC/coreBN/docs/build/html/_sources/modules.rst.txt
+++
@@ -1,7 +0,0 @@
-coreBN
-======
-
-.. toctree::
-   :maxdepth: 4
-
-   coreBN
